9defa4a48b0db8fd9ea6a9fd42283ba012cd9e9d
VIGNESH VASU <vigneshvasu0511@gmail.com>
2024-08-02 11:34:10 +0530

    Create LiveScores.java

---
+++ backend/src/main/java/com/live/backend/models/LiveScores.java
@@ -0,0 +1,35 @@
+package com.live.backend.models;
+import java.sql.Timestamp;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "live_scores")
+
+public class LiveScores {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "BIGINT(20) UNSIGNED", nullable = false)
+    private Long id;
+
+    @Column(nullable = true)
+    private Timestamp created_at;
+
+    @Column(nullable = true)
+    private Timestamp updated_at;
+
+
+}
