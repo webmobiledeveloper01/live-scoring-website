f4e2a5da830a1e3014d21b55d2872e7c53e0ed42
VIGNESH VASU <vigneshvasu0511@gmail.com>
2024-08-02 12:30:16 +0530

    Create FailedJobs.java

---
+++ backend/src/main/java/com/live/backend/models/FailedJobs.java
@@ -0,0 +1,46 @@
+package com.live.backend.models;
+
+import java.sql.Timestamp;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "failed_jobs")
+public class FailedJobs {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "BIGINT(20) UNSIGNED", nullable = false)
+    private Long id;
+
+    @Column(length = 255, nullable = false)
+    private String uuid;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String connection;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String queue;
+
+    @Column(columnDefinition = "LONGTEXT", nullable = false)
+    private String payload;
+
+    @Column(columnDefinition = "LONGTEXT", nullable = false)
+    private String exception;
+
+    @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP", nullable = true)
+    private Timestamp failed_at;
+
+}
