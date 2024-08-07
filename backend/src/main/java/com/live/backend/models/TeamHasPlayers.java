1f6c122ef5db4034b3022f0cfb40262312369f36
VIGNESH VASU <vigneshvasu0511@gmail.com>
2024-08-07 17:22:25 +0530

    Update TeamHasPlayers.java

--- backend/src/main/java/com/live/backend/models/TeamHasPlayers.java
+++ backend/src/main/java/com/live/backend/models/TeamHasPlayers.java
@@ -4,9 +4,12 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -24,11 +27,15 @@ public class TeamHasPlayers {
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "team_id", nullable = false)
-    private Long team_id;
+    @ManyToOne
+    @JoinColumn(name = "team_id", nullable = false, 
+                foreignKey = @ForeignKey(name = "fk_team_has_players_team"))
+    private Teams team;
 
-    @Column(name = "player_id", nullable = false)
-    private Long player_id;
+    @ManyToOne
+    @JoinColumn(name = "player_id", nullable = false, 
+                foreignKey = @ForeignKey(name = "fk_team_has_players_player"))
+    private Players player;
 
     @Column(name = "created_at")
     private LocalDateTime created_at;
