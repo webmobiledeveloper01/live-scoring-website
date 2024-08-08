4f3035a3a7202538744d1099facaed4382845edb
VIGNESH VASU <vigneshvasu0511@gmail.com>
2024-08-08 13:59:42 +0530

    Job batches and Tournament Sponsors

--- backend/src/main/java/com/live/backend/services/JobBatchesService.java
+++ backend/src/main/java/com/live/backend/services/JobBatchesService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.live.backend.models.JobBatches;
 import com.live.backend.repos.JobBatchesRepository;
 
@@ -24,6 +25,26 @@ public class JobBatchesService {
 
     public JobBatches save(JobBatches jobBatches) {
         return jobBatchesRepository.save(jobBatches);
+    }
+
+    public JobBatches updateJobBatches(String id, JobBatches jobBatches) {
+        Optional<JobBatches> existingJobBatchOpt = jobBatchesRepository.findById(id);
+        if (existingJobBatchOpt.isPresent()) {
+            JobBatches existingJobBatch = existingJobBatchOpt.get();
+            existingJobBatch.setName(jobBatches.getName());
+            existingJobBatch.setTotal_jobs(jobBatches.getTotal_jobs());
+            existingJobBatch.setPending_jobs(jobBatches.getPending_jobs());
+            existingJobBatch.setFailed_jobs(jobBatches.getFailed_jobs());
+            existingJobBatch.setFailed_job_ids(jobBatches.getFailed_job_ids());
+            existingJobBatch.setOptions(jobBatches.getOptions());
+            existingJobBatch.setCancelled_at(jobBatches.getCancelled_at());
+            existingJobBatch.setCreated_at(jobBatches.getCreated_at());
+            existingJobBatch.setFinished_at(jobBatches.getFinished_at());
+
+            return jobBatchesRepository.save(existingJobBatch);
+        } else {
+            throw new RuntimeException("JobBatches not found with ID: " + id);
+        }
     }
 
     public void deleteById(String id) {
