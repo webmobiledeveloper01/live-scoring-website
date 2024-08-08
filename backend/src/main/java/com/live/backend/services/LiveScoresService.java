294926c4d43cbf7bff1ea5817ef0cddf976eadfd
VIGNESH VASU <vigneshvasu0511@gmail.com>
2024-08-08 20:03:24 +0530

    update Live Score and Cache

--- backend/src/main/java/com/live/backend/services/LiveScoresService.java
+++ backend/src/main/java/com/live/backend/services/LiveScoresService.java
@@ -1,11 +1,12 @@
 package com.live.backend.services;
 
-
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.live.backend.models.LiveScores;
 import com.live.backend.repos.LiveScoresRepository;
 
@@ -24,7 +25,21 @@ public class LiveScoresService {
     }
 
     public LiveScores save(LiveScores liveScores) {
+        liveScores.setCreated_at(LocalDateTime.now());
+        liveScores.setUpdated_at(LocalDateTime.now());
         return liveScoresRepository.save(liveScores);
+    }
+
+    public LiveScores updateLiveScores(Long id, LiveScores liveScores) {
+        Optional<LiveScores> existingLiveScoresOpt = liveScoresRepository.findById(id);
+        if (existingLiveScoresOpt.isPresent()) {
+            LiveScores existingLiveScores = existingLiveScoresOpt.get();
+            existingLiveScores.setUpdated_at(LocalDateTime.now());
+
+            return liveScoresRepository.save(existingLiveScores);
+        } else {
+            throw new RuntimeException("LiveScores not found with ID: " + id);
+        }
     }
 
     public void deleteById(Long id) {
--- backend/src/main/java/com/live/backend/services/LocalDateTime.java
+++
@@ -1,5 +0,0 @@
-package com.live.backend.services;
-
-public class LocalDateTime {
-
-}
